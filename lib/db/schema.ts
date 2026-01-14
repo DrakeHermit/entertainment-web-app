70889b5a6065fc17e8e886dbeb635560c77cbaf9
Jan Kotvaš <jan.kotvas96@gmail.com>
2026-01-14 20:16:13 +0100

    feat: added postgresdb connection with drizzle orm

---
+++ lib/db/schema.ts
@@ -0,0 +1,9 @@
+import { integer, text, timestamp, pgTable } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    email: text().notNull().unique(),
+    password: text().notNull(),
+    createdAt: timestamp().notNull().defaultNow(),
+    updatedAt: timestamp().notNull().defaultNow(),
+});
